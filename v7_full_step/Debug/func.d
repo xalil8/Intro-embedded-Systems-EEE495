c0686e63b755e55b031d02718b90a73fdfcdaa50
halil özcan <46287166+xalil8@users.noreply.github.com>
2022-12-31 21:48:03 +0300

    Add files via upload

---
+++ v7_full_step/Debug/func.d
@@ -0,0 +1,24 @@
+# FIXED
+
+func.obj: ../func.c
+func.obj: C:/Users/halil/Desktop/embeded/v6_final/my_motor_lib.h
+func.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430.h
+func.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430g2533.h
+func.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/in430.h
+func.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+func.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+
+../func.c:
+
+C:/Users/halil/Desktop/embeded/v6_final/my_motor_lib.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430g2533.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
