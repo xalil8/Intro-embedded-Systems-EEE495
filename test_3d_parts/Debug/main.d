5b2bfe423ac87d8f757c832ddad8feb1f568b3ef
halil özcan <46287166+xalil8@users.noreply.github.com>
2023-01-05 12:36:37 +0300

    Add files via upload

---
+++ test_3d_parts/Debug/main.d
@@ -0,0 +1,27 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+main.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+main.obj: C:/Users/halil/Desktop/embeded/v6_final/my_motor_lib.h
+main.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430g2533.h
+
+../main.c:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430g2553.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/Users/halil/Desktop/embeded/v6_final/my_motor_lib.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430g2533.h:
+
---
+++ test_3d_parts/Debug/my_motor_lib.d
@@ -0,0 +1,24 @@
+# FIXED
+
+my_motor_lib.obj: ../my_motor_lib.c
+my_motor_lib.obj: C:/Users/halil/Desktop/embeded/v6_final/my_motor_lib.h
+my_motor_lib.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430.h
+my_motor_lib.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430g2533.h
+my_motor_lib.obj: C:/ti/ccs1200/ccs/ccs_base/msp430/include/in430.h
+my_motor_lib.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+my_motor_lib.obj: C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+
+../my_motor_lib.c:
+
+C:/Users/halil/Desktop/embeded/v6_final/my_motor_lib.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/msp430g2533.h:
+
+C:/ti/ccs1200/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1200/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
